e287257e59f00253547fa383442b98040f8218be
Peter Pimley <peter.pimley@unity3d.com>
2020-08-11 22:13:01 +0100

    Very basic functionality

--- Runtime/AppLogOnText.cs
+++ Runtime/AppLogOnText.cs
@@ -1,8 +1,55 @@
 
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 
-class AppLogOnText : MonoBehaviour
-{}
+
+namespace org.peterpi.debugtools
+{
+
+
+	[RequireComponent(typeof(UnityEngine.UI.Text))]
+	class AppLogOnText : MonoBehaviour
+	{
+		private Text m_t;
+
+
+		[SerializeField]
+		private int m_capacity = 20;
+
+		private List<string> m_lines = new List<string>();
+
+
+		private void Awake()
+		{
+			m_t = GetComponent<Text>();
+			Debug.Assert(m_t != null);
+			Application.logMessageReceived += Log;
+
+			Debug.Log("test");
+		}
+
+		private void OnDestroy()
+		{
+			Application.logMessageReceived -= Log;
+		}
+
+		private void Log (string mesg, string stack, LogType lt)
+		{
+			m_lines.Add(mesg);
+			int overflow = m_lines.Count - m_capacity;
+			if (overflow > 0)
+			{
+				m_lines.RemoveRange(0, overflow);
+			}
+
+			string s = string.Join("\n", m_lines);
+			m_t.text = s;
+		}
+	}
+
+
+}
 
 
