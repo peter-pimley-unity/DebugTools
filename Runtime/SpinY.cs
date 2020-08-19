0b6b20529212a00c5262d98cdf23cd0441036a4e
Peter Pimley <peter.pimley@unity3d.com>
2020-08-19 17:49:52 +0100

    Added a script to spin around Y. This is useful on a cube just to show the frame rate. Bumped version accordingly.

---
+++ Runtime/SpinY.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SpinY : MonoBehaviour
+{
+	[SerializeField]
+	private float m_speed = 90.0f; // degrees per second.
+
+	// Update is called once per frame
+	void Update()
+	{
+		float theta = m_speed * Time.deltaTime;
+		transform.RotateAround(Vector3.up, theta * Mathf.Deg2Rad);
+	}
+}
